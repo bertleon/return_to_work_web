dd9862adc59ad650b077034936850f78ba41c7e2
Roberto Leon-Luna <bertleon@umich.edu>
2020-04-19 17:29:11 -0400

    testing v-for

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,12 @@
 var mainView = new Vue({
     el: "#main",
     data: {
-        questions: []
+        questions: [], 
+        answers: {}
     },
     mounted: function() {
         axios.get("./json/questions.json").then(response => {
             this.questions = response.data;
-            console.log(this.questions);
         });
     },
     methods: {
